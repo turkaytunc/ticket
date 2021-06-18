edb71d343159ccdb501103ee1ff9010919fc13fe
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-18 15:02:20 +0300

    add: ingress-nginx config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('api/users/currentuser', (req, res) => {
+  res.send('Hello from express');
+});
+
 app.listen(3000, () => {
-  console.log('Auth => http://localhost:3000');
+  console.log('Auth Service => http://localhost:3000');
 });
