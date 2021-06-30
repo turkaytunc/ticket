55c7b8677f7ae6b4c5d8f1bd3e468fb1a1a752e8
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-30 19:10:49 +0300

    add: request validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    res.send('hey');
+  }
+);
 
 export default router;
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models';
-import { HttpError, RequestValidationError } from '../utils';
 import jwt from 'jsonwebtoken';
+import { body } from 'express-validator';
+import { User } from '../models';
+import { HttpError } from '../utils';
+import { validateRequest } from '../middlewares';
 
 const router = express.Router();
 
@@ -12,13 +13,9 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-      }
       const { email, password } = req.body;
 
       const isUserExists = await User.findOne({ email });
