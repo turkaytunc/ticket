eeffe0727bf42c4a546fbdd4e68c487505866726
Türkay Tunç <turkaytunc00@gmail.com>
2021-07-23 14:08:15 +0300

    add: test setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,23 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: MongoMemoryServer;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'kjsdf13';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
