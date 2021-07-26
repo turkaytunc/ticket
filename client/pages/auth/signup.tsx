46b36bd93d6b75c378120a0bf08d4fe1049b541a
Türkay Tunç <turkaytunc00@gmail.com>
2021-07-26 20:49:12 +0300

    fix: anonymous function lint error

---
+++ client/pages/auth/signup.tsx
@@ -0,0 +1,9 @@
+const signup = () => {
+  return (
+    <div>
+      <h2>Sign up</h2>
+    </div>
+  );
+};
+
+export default signup;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <div>
-      index file <button className="btn btn-danger">hey</button>
+      index file <button className="btn btn-primary">hey</button>
     </div>
   );
 };
