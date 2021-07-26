9ea61f4ea781ce5a7d0c48040580858c73c1f5a7
Türkay Tunç <turkaytunc00@gmail.com>
2021-07-26 20:44:13 +0300

    add: bootstrap and nextjs configs

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,9 @@
 const Main = () => {
-  return <div>index file</div>;
+  return (
+    <div>
+      index file <button className="btn btn-danger">hey</button>
+    </div>
+  );
 };
 
 export default Main;
